60db2722ea77fa6df356ddb8f6620694cb48bcbc
jayendradevaiya <161676471+jayendradevaiya@users.noreply.github.com>
2025-12-11 05:14:33 +0000

    condition

---
+++ 02_conditionals/snak_suggestion.py
@@ -0,0 +1,7 @@
+snack = input("Enter your preferred snake: ").lower()
+
+if snack == "cookies" or snack == "samosa":
+    print(f"Greate choice! we'll serve you {snack}")
+
+else:
+    print(f"Sorry, we onlt serve cookies of samosa with tea")
