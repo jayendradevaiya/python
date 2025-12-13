c4ca27684654c2b4944925b7b5376f0ece4c3fcc
jayendradevaiya <161676471+jayendradevaiya@users.noreply.github.com>
2025-12-13 15:50:40 +0000

    functions

---
+++ 04_function/02_complax.py
@@ -0,0 +1,17 @@
+def fatch_sales():
+    print("Fetching the sales data")
+
+    
+def filter_valid_sales():
+    print("Filtering valid sales data")
+
+def summerize_data():
+    print("Summarizing sales data")
+
+def generate_report():
+    fatch_sales()
+    filter_valid_sales()
+    summerize_data()
+    print("Report is ready")
+
+generate_report()
