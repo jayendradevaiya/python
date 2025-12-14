d63fccd0a1f8277e8629768b162cc937b6302fcf
jayendradevaiya <161676471+jayendradevaiya@users.noreply.github.com>
2025-12-14 14:10:55 +0000

    decorators

---
+++ 07_generators/02_infinite_generators.py
@@ -0,0 +1,14 @@
+def infinte_chai():
+    count = 1
+    while True:
+        yield f"Refil #{count}"
+        count += 1
+
+rerill = infinte_chai()
+user2 = infinte_chai()
+
+for _ in range(10):
+    print(next(rerill))
+
+for _ in range(6):
+    print(next(user2))
